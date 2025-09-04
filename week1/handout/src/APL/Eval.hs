dd84a350694f1e18bbb8c465487797a8b62483f9
RosaMeyer <jdc334@alumni.ku.dk>
2025-09-04 11:59:55 +0200

    First exersice class

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,6 +1,26 @@
 module APL.Eval
   (
+    Val(..),
+    eval
   )
 where
 
-import APL.AST ()
+import APL.AST (Exp(..))
+
+data Val
+  = ValInt Integer
+  deriving (Eq, Show)
+
+type Error = String
+
+eval :: Exp -> Either Error Val
+eval (CstInt x) = Right (ValInt x)
+eval (Sub (CstInt x) (CstInt y)) = Right (ValInt (x - y))
+eval (Add (CstInt x) (CstInt y)) = Right (ValInt (x + y))
+eval (Mul (CstInt x) (CstInt y)) = Right (ValInt (x * y))
+eval (Div (CstInt _) (CstInt 0)) = Left ("Division by 0")
+eval (Div (CstInt x) (CstInt y)) = Right (ValInt (div x y))  --  (x `div`y)
+eval (Pow (CstInt x) (CstInt y)) 
+  | y < 0  = Left ("Exponent negative")
+  | y >= 0 = Right (ValInt (x ^ y))
+-- eval (Pow (CstInt x) (CstInt y)) = Right (ValInt (x ^ y))
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,12 +1,43 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST ()
-import APL.Eval ()
+import APL.AST (Exp(..))
+import APL.Eval (Val(..), eval)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit ()
+import Test.Tasty.HUnit (testCase, (@?=))
 
 tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [ testCase "eval" $ 
+        eval (CstInt 5) 
+        @?= (Right (ValInt 5)),
+      
+      testCase "Add" $
+        eval (Add (CstInt 5) (CstInt 2))
+        @?= (Right (ValInt 7)),
+
+      testCase "Sub" $
+        eval (Sub (CstInt 5) (CstInt 2))
+        @?= (Right (ValInt 3)),
+
+      testCase "Mul" $
+        eval (Mul (CstInt 5) (CstInt 2))
+        @?= (Right (ValInt 10)),
+
+      testCase "Div_by_0" $
+        eval (Div (CstInt 6) (CstInt 0))
+        @?= (Left "Division by 0"),
+
+      testCase "Div" $
+        eval (Div (CstInt 6) (CstInt 2))
+        @?= (Right (ValInt 3)),
+
+      testCase "Pow_by_0" $
+        eval (Pow (CstInt 5) (CstInt (-2)))
+        @?= (Left "Exponent negative"),
+
+      testCase "Pow" $
+        eval (Pow (CstInt 5) (CstInt 2))
+        @?= (Right (ValInt 25))
+    ]
